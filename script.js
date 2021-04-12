d59cf93df5558da361cad8c78fac3aa4d617395d
GalinaSherbak84 <galinasherbak@gmail.com>
2021-04-12 16:35:01 +0200

    timer

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
-    
-    
+//  5 numeri generati casualmente.
+    var numeri = 5 ;
+    var numberList =[];
+    while(numberList.length < numeri){
+        var number = randomNamber(1, 100);
+        if(! numberList.includes(number)){
+            numberList.push(number);
+        }
+    }
+// Alert
+    alert(numberList );
+// Timer di 30 secondi.
+    var time = 0;
+    var timeEnd = setInterval(function(){
+        if(time == 30){
+            clearInterval(timeEnd);
+        }else{
+            time++;
+            console.log(time);
+        }
+    }, 1000);
         
-    //fine document ready    
+// fine document ready    
     });
+
+// funzione numeri random
+    function randomNamber(min, max){
+        return Math.floor(Math.random()*(max - min + 1) ) + min;
+    } 
